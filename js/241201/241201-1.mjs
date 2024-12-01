9b48f89839c9c2cae3f5395812d14c0fd207f250
Micoko <112613372+rakaso598@users.noreply.github.com>
2024-12-01 23:38:22 +0900

    241201-1

---
+++ js/241201/241201-1.mjs
@@ -0,0 +1,6 @@
+import multiply, { add, subtract, PI } from './mathUtils.mjs';
+
+console.log(`Addition: ${add(5, 3)}`);
+console.log(`Subtraction: ${subtract(5, 3)}`);
+console.log(`PI: ${PI}`);
+console.log(`Multiplication: ${multiply(5, 3)}`);
---
+++ js/241201/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+import { add, subtract, PI } from './mathUtils.js';
+
+console.log(`Addition: ${add(5, 3)}`);
+console.log(`Subtraction: ${subtract(5, 3)}`);
+console.log(`PI: ${PI}`);

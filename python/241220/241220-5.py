786a5f0994aa3a8009ad8b62f11e8f47f39523cb
rakaso598 <nexxus86@outlook.com>
2024-12-20 12:42:30 +0900

    241220-5

--- python/241220/241220-5.py
+++ python/241220/241220-5.py
@@ -3,7 +3,7 @@ import pandas as pd
 samsong_df = pd.read_csv('data/samsong.csv')
 hyundee_df = pd.read_csv('data/hyundee.csv')
 
-# 사람들의 요일별 문화생활비 merge
+# 고객분석 - 사람들의 요일별 문화생활비 (pd.merge)
 dfs=samsong_df.loc[:,['요일','문화생활비']]
 dfh=hyundee_df.loc[:,['요일','문화생활비']]
 

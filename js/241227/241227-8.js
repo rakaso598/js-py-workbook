6679b3359ed59f3456eb25a3e2e9f2e5c6a81a98
rakaso598 <nexxus86@outlook.com>
2024-12-27 19:14:01 +0900

    241227-4-pre

---
+++ js/241227/241227-8.js
@@ -0,0 +1,12 @@
+// 반복이 많은 작업
+function logParticipant(name){
+    console.log(`${name}(이)가 대화에 참여했습니다.`);
+  }
+  
+// 테스트 코드
+logParticipant('동수');
+logParticipant('윤하');
+logParticipant('재준');
+logParticipant('동훈');
+logParticipant('영희');
+logParticipant('신욱');
---
+++ js/241227/241227-9.js
@@ -0,0 +1,12 @@
+// 반복이 많은 작업
+function expressMultiplication(one, two){
+    console.log(`${one} * ${two} = ` + one * two);
+  }
+  
+// 테스트 코드
+expressMultiplication(3, 4);
+expressMultiplication(3, 2);
+expressMultiplication(7, 5);
+expressMultiplication(8, 9);
+expressMultiplication(5, 5);
+expressMultiplication(9, 9);

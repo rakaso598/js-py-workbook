81484d48a1b9a186be5264dc73d58b2d9d1e6200
rakaso598 <nexxus86@outlook.com>
2024-12-31 14:29:37 +0900

    241231-2-math

---
+++ js/241231/241231-2.js
@@ -0,0 +1,39 @@
+// 절댓값 (Absolute Value)
+console.log(Math.abs(-10));
+console.log(Math.abs(10));
+
+// 최댓값 (Maximum)
+console.log(Math.max(2, -1, 4, 5, 0));
+
+// 최솟값 (Minimum)
+console.log(Math.min(2, -1, 4, 5, 0));
+
+// 거듭제곱 (Exponentiation)
+console.log(Math.pow(2, 3));
+console.log(Math.pow(5, 2));
+
+// 제곱근 (Square Root)
+console.log(Math.sqrt(25));
+console.log(Math.sqrt(49));
+
+// 반올림 (Round)
+console.log(Math.round(2.3));
+console.log(Math.round(2.4));
+console.log(Math.round(2.49));
+console.log(Math.round(2.5));
+console.log(Math.round(2.6));
+
+// 버림과 올림 (Floor and Ceil)
+console.log(Math.floor(2.4));
+console.log(Math.floor(2.49));
+console.log(Math.floor(2.8));
+console.log('-');
+console.log(Math.ceil(2.4));
+console.log(Math.ceil(2.49));
+console.log(Math.ceil(2.8));
+
+// 난수 (Random)
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
